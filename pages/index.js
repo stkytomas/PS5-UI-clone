01b9378c0116f42d0304b08668812461d990227f
stkytomas <stkytomas@gmail.com>
2021-01-27 15:37:37 -0600

    added search, user image and time

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,27 @@ export default function Home() {
     return ( 
     <div className = "screen">
         <header>
-            <div className="media-nav">
+            <section className="media-nav">
                 <a href="#" className="active">
                     Games
                 </a>
                 <a href="#">Media</a>
-            </div>
+            </section>
+            <section className="settings">
+                <span className="icon">
+                    <i className="fa fa-search"></i>
+                </span>
+                <span className="icon">
+                    <i className="fa fa-cog"></i>
+                </span>
+                <span className="icon">
+                    <img 
+                    className="user"
+                    src="https://www.rollingstone.com/wp-content/uploads/2021/01/bernie-meme-photog.jpg"
+                    />
+                </span>
+                <span className="time">3:36 PM</span>
+            </section>
         </header>
     </div>
     )
